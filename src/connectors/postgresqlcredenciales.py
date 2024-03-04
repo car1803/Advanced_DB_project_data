5eedf70fe3fc5639b5f359260e1b786cce265829
ycuervob <ycuervob@unal.edu.co>
2024-03-03 20:27:54 -0500

    Cambio .env

--- src/connectors/postgresqlcredenciales.py
+++ src/connectors/postgresqlcredenciales.py
@@ -1,8 +1,15 @@
+from dotenv import load_dotenv
+import os
+
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
+
+# Acceder a las variables de entorno cargadas
 postgresCredentials = {
-    "host":"35.237.42.142",
-    "database":"egresados",
-    "user":"postgres",
-    "password":"665RE(v&x>iVL;6<"
+    "host": os.getenv("POSTGRES_HOST"),
+    "database": os.getenv("POSTGRES_DATABASE"),
+    "user": os.getenv("POSTGRES_USER"),
+    "password": os.getenv("POSTGRES_PASSWORD")
 }
 
 postgresUri = f"postgresql://{postgresCredentials['user']}:{postgresCredentials['password']}@{postgresCredentials['host']}/{postgresCredentials['database']}"
