e93b00bb3461798b03553ef928df8b85e25263e5
ycuervob <ycuervob@unal.edu.co>
2024-04-24 19:10:04 -0500

    Mas tiempo
    Le agrego mas tiempo porque a lo mejor tengo un pc de la nasa :v

--- docker/start-system.py
+++ docker/start-system.py
@@ -5,20 +5,20 @@ def execute_command(service, command, port):
     print("Iniciando docker-compose...")
     subprocess.run(["docker-compose", "up", "-d"])
 
-    print("Esperando 10 segundos para iniciar os serviços...")
-    time.sleep(10)
-    
+    print("Esperando 20 segundos para iniciar os serviços...")
+    time.sleep(20)
+
     subprocess.run(["docker-compose", "exec", service, "mongosh", f'--port={port}', "--eval", command])
 
 def main():
-    print("configundo configsvr ","-" * 20)
+    print("configundo configsvr ","-" * 40)
     execute_command("configsvr", 'rs.initiate({"_id": "rs-config-server", "configsvr": true, "members": [{"_id": 0, "host": "configsvr:27017"}]})', 27017)
 
-    print("configurando shard1 ","-" * 20)
+    print("configurando shard1 ","-" * 40)
     execute_command("shard1a", 'rs.initiate({"_id": "shard1", "members": [{"_id": 0, "host": "shard1a:27018"}, {"_id": 1, "host": "shard1b:27018"}]})', 27018)
     execute_command("shard1b", 'rs.initiate({"_id": "shard1", "members": [{"_id": 0, "host": "shard1a:27018"}, {"_id": 1, "host": "shard1b:27018"}]})', 27018)
 
-    print("configurando mongos ","-" * 20)
+    print("configurando mongos ","-" * 40)
     execute_command("mongos", 'sh.addShard("shard1/shard1a:27018")', 27017)
     execute_command("mongos", 'sh.addShard("shard1/shard1b:27018")', 27017)
 
