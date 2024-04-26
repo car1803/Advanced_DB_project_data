5152b20a2ed60d7cd3761b98875d3dff2528e6d5
ycuervob <ycuervob@unal.edu.co>
2024-04-26 07:56:09 -0500

    Ahora si se vino los shards
    Puede que genere algún probleama con otros os en ese caso solo correr la función main del archivo de python una única vez levantados los contenedores y no más.

--- docker/start-system.py
+++ docker/start-system.py
@@ -1,5 +1,9 @@
 import subprocess
 import time
+import os
+
+
+data_server_config = "configsvr-data"
 
 def execute_command(service, command, port):
     subprocess.run(["docker-compose", "exec", service, "mongosh", f'--port={port}', "--eval", command])
@@ -11,15 +15,31 @@ def main():
     print("configurando shard1 ","-" * 40)
     execute_command("shard1a", 'rs.initiate({"_id": "shard1", "members": [{"_id": 0, "host": "shard1a:27018"}, {"_id": 1, "host": "shard1b:27018"}]})', 27018)
     execute_command("shard1b", 'rs.initiate({"_id": "shard1", "members": [{"_id": 0, "host": "shard1a:27018"}, {"_id": 1, "host": "shard1b:27018"}]})', 27018)
+    execute_command("shard2a", 'rs.initiate({"_id": "shard2", "members": [{"_id": 0, "host": "shard2a:27018"}, {"_id": 1, "host": "shard2b:27018"}]})', 27018)
+    execute_command("shard2b", 'rs.initiate({"_id": "shard2", "members": [{"_id": 0, "host": "shard2a:27018"}, {"_id": 1, "host": "shard2b:27018"}]})', 27018)
 
     print("configurando mongos ","-" * 40)
     execute_command("mongos", 'sh.addShard("shard1/shard1a:27018")', 27017)
     execute_command("mongos", 'sh.addShard("shard1/shard1b:27018")', 27017)
+    execute_command("mongos", 'sh.addShard("shard2/shard2a:27018")', 27017)
+    execute_command("mongos", 'sh.addShard("shard2/shard2b:27018")', 27017)
+
+    print("configurando colecciones ","-" * 40)
+    execute_command("mongos", 'sh.enableSharding("egresados")', 27017)
+    execute_command("mongos", 'sh.shardCollection("egresados.htrabajoestudiante", {"id": 1})', 27017)
+    execute_command("mongos", 'sh.shardCollection("egresados.hestudianteidioma", {"id": 1})', 27017)
+    execute_command("mongos", 'sh.shardCollection("egresados.hempresa", {"id": 1})', 27017)
 
 if __name__ == "__main__":
-    print("Iniciando docker-compose...")
-    subprocess.run(["docker-compose", "up", "-d"])
-
-    print("Esperando 20 segundos para iniciar os serviços...")
-    time.sleep(20)
-    main()
+    ruta_actual = os.getcwd()
+    ruta_completa = os.path.join(ruta_actual, data_server_config)
+    if os.path.exists(ruta_completa) and os.path.isdir(ruta_completa):
+        print("Iniciando docker-compose...")
+        subprocess.run(["docker-compose", "up", "-d"])
+        print(f"Ya se ha creado la carpeta de datos de configsrv no es necesario correr la migración.")
+    else:
+        print("Iniciando docker-compose...")
+        subprocess.run(["docker-compose", "up", "-d"])
+        print("Esperando 20 segundos para iniciar los servicios...")
+        time.sleep(20)
+        main()
