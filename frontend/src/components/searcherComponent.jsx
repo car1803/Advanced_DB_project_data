7a9096921dc69a074c9283c3d6eb653527ec41a2
Felipe Rojas <juancendales@periferia-it.com>
2024-06-08 15:29:52 -0500

    Add collection division in searcher

--- frontend/src/components/searcherComponent.jsx
+++ frontend/src/components/searcherComponent.jsx
@@ -1,22 +1,22 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { Button, Container, Form, Row } from 'react-bootstrap';
-import glovars  from '../glovars';
+import glovars from '../glovars';
 import Select from 'react-select';
 
 const SearcherComponent = ({ DataTableComponent, ModalComponent }) => {
-    const [searchTerm, setSearchTerm]  = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
     const [data, setData] = useState(null);
     const [collectionSelected, setCollectionSelected] = useState(null);
     const [collectionSelectedDummy, setCollectionSelectedDummy] = useState(null);
     const [showTable, setShowTable] = useState(false);
-    const [ showModal, setShowModal ] = useState(false);
-    const [ modalData ] = useState({ title: 'Error', description: 'Por favor, selecciona una colección y escribe un término de búsqueda' });
+    const [showModal, setShowModal] = useState(false);
+    const [modalData] = useState({ title: 'Error', description: 'Por favor, selecciona una colección y escribe un término de búsqueda' });
 
     useEffect(() => {
         let midata = ["Cargando..."];
         setData(midata);
-        
+
         axios.get(`${glovars.backendUrl}/getCollections`).then((response) => {
             setData(response.data);
         }).catch((error) => {
@@ -24,7 +24,7 @@ const SearcherComponent = ({ DataTableComponent, ModalComponent }) => {
         });
 
         setShowTable(false);
-      }, []); 
+    }, []);
 
     const handleSearch = (e) => {
         if (!searchTerm || !collectionSelectedDummy) {
@@ -40,8 +40,19 @@ const SearcherComponent = ({ DataTableComponent, ModalComponent }) => {
         console.log('Collection selected:', collectionSelectedDummy);
     };
 
+    const groupedOptions = data && [
+        {
+            label: 'Colecciones de hecho, RI habilitado.',
+            options: data.filter(item => item.toLowerCase().startsWith('h')).map(item => ({ value: item, label: item }))
+        },
+        {
+            label: 'Otras colecciones.',
+            options: data.filter(item => !item.toLowerCase().startsWith('h')).map(item => ({ value: item, label: item }))
+        }
+    ];
+
     return (
-        <Container className ='text-start'>
+        <Container className='text-start'>
             <Row>
                 <h1 className='m-4'>Buscador:</h1>
                 <Form className='border p-4 rounded is-invalid' onSubmit={handleSearch}>
@@ -57,22 +68,22 @@ const SearcherComponent = ({ DataTableComponent, ModalComponent }) => {
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Select
-                            options={data && data.map((item, index) => ( { value: item, label: item }))}
+                            options={groupedOptions}
                             onChange={(e) => setCollectionSelectedDummy(e.value)}
                             placeholder="--Selecciona una colección--"
-                            />
+                        />
                     </Form.Group>
 
                     <Button variant="primary" type="submit">Buscar</Button>
                 </Form>
             </Row>
-            { showTable && 
+            {showTable &&
                 <Row>
-                    <DataTableComponent collectionName = { collectionSelected } ModalComponent={ ModalComponent } itemsPerPage={10} />
+                    <DataTableComponent collectionName={collectionSelected} ModalComponent={ModalComponent} itemsPerPage={10} />
                 </Row>
             }
 
-            <ModalComponent setShowModal={setShowModal} show = {showModal} title = { modalData.title } description = { modalData.description } />
+            <ModalComponent setShowModal={setShowModal} show={showModal} title={modalData.title} description={modalData.description} />
         </Container>
     );
 };
