5644f67a956b857998c160f8b2af3a01405e1b64
Felipe Rojas <juancendales@periferia-it.com>
2024-06-09 14:40:52 -0500

    Add metabase in search

--- frontend/src/components/dataTableComponent.jsx
+++ frontend/src/components/dataTableComponent.jsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Table, Pagination, FormControl, InputGroup, Button, Alert } from 'react-bootstrap';
+import MetabaseComponent from './metabaseComponent';
 
 function isObject(value) {
   return value !== null && typeof value === 'object';
@@ -77,6 +78,25 @@ const DataTableComponent = ({ collectionName, itemsPerPage, searchTerm, ModalCom
     setCurrentPage(pageNumber);
   };
 
+  const metabaseCollectionsDictionary = {
+    "salario_promedio_por_sector": "http://localhost:3300/public/question/19f8095f-153d-4333-9cb8-ec7ae876d8f3", 
+    "salario_promedio_por_pais": "http://localhost:3300/public/question/fa1faa8a-4734-43d8-bd37-e78b051cc2cf", 
+    "salario_promedio_por_genero": "http://localhost:3300/public/question/5bdd4288-f222-4595-b024-5f2a9ead3e0e",
+    "salario_promedio_por_cargo": "http://localhost:3300/public/question/6907bb79-6a74-4712-91d9-50c53de3b1b8",
+    "promedio_nivel_por_idioma": "http://localhost:3300/public/question/bb1d72a6-cdd2-452b-a4ee-eea20630401b", 
+    "promedio_gasto_salarios_por_sector": "http://localhost:3300/public/question/19f8095f-153d-4333-9cb8-ec7ae876d8f3", 
+    "palabras_mas_comunes_en_cargos":"http://localhost:3300/public/question/bb1d72a6-cdd2-452b-a4ee-eea20630401b",
+    "nombre_empresas_mas_frecuente":"http://localhost:3300/public/question/235d6438-8440-48a0-a217-a203701d62bc",
+    "niveles_idioma_mas_comunes":"http://localhost:3300/public/question/e7b6a07f-764d-4a7d-9947-0fcb9f22f44d",
+    "estudiantes_por_pais":"http://localhost:3300/public/question/b33411f5-e33d-49d6-b549-8ea0d847fa3f",
+    "estudiantes_por_genero":"http://localhost:3300/public/question/5d048564-05b0-47ee-9d1c-4f7405cbb039",
+    "empresa_por_tipo":"http://localhost:3300/public/question/e4a7559d-2c80-4915-aaa0-8228b4e91f9f",
+    "edad_promedio_egresados":"http://localhost:3300/public/question/373aa53d-8407-478f-904a-f2acde8b01a5",  
+    "carreras_mas_comunes_en_empresas":"http://localhost:3300/public/question/ec7c0b1e-7fd8-4b71-8497-3f165c6cb20a", 
+    "cantidad_personas_por_genero_y_sector":"http://localhost:3300/public/question/a1d98273-ccb6-4887-8aa8-76efc7d7097d",
+    "cantidad_estudiantes_por_idioma":"http://localhost:3300/public/question/0f2e30df-c8e6-42aa-8cf6-ecf66ded2af3",
+  }
+
   const totalPages = Math.ceil(filteredData.length / itemsPerPage);
   const displayedData = filteredData.slice(
     (currentPage - 1) * itemsPerPage,
@@ -95,6 +115,7 @@ const DataTableComponent = ({ collectionName, itemsPerPage, searchTerm, ModalCom
         />
       </InputGroup>
       <ModalComponent title={modalData.title} description={modalData.description} show={showModal} setShowModal={setShowModal} />
+      <MetabaseComponent src={metabaseCollectionsDictionary[collectionName] || null} onlyIframe={true}/>
       <div style={{ "overflow-x": "auto", "border": "2px solid #ccc"}}>
         <Table style={{ "white-space": "nowrap" }} striped bordered hover>
           <thead>
--- frontend/src/components/metabaseComponent.jsx
+++ frontend/src/components/metabaseComponent.jsx
@@ -2,17 +2,35 @@ import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 import { useEffect } from 'react';
 
-const MetabaseComponent = () => {
+const MetabaseComponent = ({ src, onlyIframe }) => {
     useEffect(() => {
         console.log('MetabaseComponent mounted');
     }, []);
-    
+
+    console.log(src, onlyIframe)
+
+    if (!src) {
+        return null;
+    }
+
+    if (onlyIframe) {
+        return (
+            <iframe
+                    src={src}
+                    title="Metabase Dashboard"
+                    width="100%"
+                    height="800"
+                    allowFullScreen
+            ></iframe>
+        )
+    }
+
     return (
         <Container>
             <h2 className='m-4'>Información resumida:</h2>
             <Row>
                 <iframe
-                    src="http://localhost:3300/public/dashboard/df605c0e-186f-4b34-828f-7d350fe6e801"
+                    src={src}
                     title="Metabase Dashboard"
                     width="100%"
                     height="800"
