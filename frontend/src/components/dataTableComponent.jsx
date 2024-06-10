5c724f1972720fc434e426b989c8c12c384559b9
Felipe Rojas <juancendales@periferia-it.com>
2024-06-10 14:07:07 -0500

    Change links and metabase db

--- frontend/src/components/dataTableComponent.jsx
+++ frontend/src/components/dataTableComponent.jsx
@@ -79,22 +79,22 @@ const DataTableComponent = ({ collectionName, itemsPerPage, searchTerm, ModalCom
   };
 
   const metabaseCollectionsDictionary = {
-    "salario_promedio_por_sector": "http://localhost:3300/public/question/19f8095f-153d-4333-9cb8-ec7ae876d8f3", 
-    "salario_promedio_por_pais": "http://localhost:3300/public/question/fa1faa8a-4734-43d8-bd37-e78b051cc2cf", 
-    "salario_promedio_por_genero": "http://localhost:3300/public/question/5bdd4288-f222-4595-b024-5f2a9ead3e0e",
-    "salario_promedio_por_cargo": "http://localhost:3300/public/question/6907bb79-6a74-4712-91d9-50c53de3b1b8",
-    "promedio_nivel_por_idioma": "http://localhost:3300/public/question/bb1d72a6-cdd2-452b-a4ee-eea20630401b", 
-    "promedio_gasto_salarios_por_sector": "http://localhost:3300/public/question/19f8095f-153d-4333-9cb8-ec7ae876d8f3", 
-    "palabras_mas_comunes_en_cargos":"http://localhost:3300/public/question/bb1d72a6-cdd2-452b-a4ee-eea20630401b",
-    "nombre_empresas_mas_frecuente":"http://localhost:3300/public/question/235d6438-8440-48a0-a217-a203701d62bc",
-    "niveles_idioma_mas_comunes":"http://localhost:3300/public/question/e7b6a07f-764d-4a7d-9947-0fcb9f22f44d",
-    "estudiantes_por_pais":"http://localhost:3300/public/question/b33411f5-e33d-49d6-b549-8ea0d847fa3f",
-    "estudiantes_por_genero":"http://localhost:3300/public/question/5d048564-05b0-47ee-9d1c-4f7405cbb039",
-    "empresa_por_tipo":"http://localhost:3300/public/question/e4a7559d-2c80-4915-aaa0-8228b4e91f9f",
-    "edad_promedio_egresados":"http://localhost:3300/public/question/373aa53d-8407-478f-904a-f2acde8b01a5",  
-    "carreras_mas_comunes_en_empresas":"http://localhost:3300/public/question/ec7c0b1e-7fd8-4b71-8497-3f165c6cb20a", 
-    "cantidad_personas_por_genero_y_sector":"http://localhost:3300/public/question/a1d98273-ccb6-4887-8aa8-76efc7d7097d",
-    "cantidad_estudiantes_por_idioma":"http://localhost:3300/public/question/0f2e30df-c8e6-42aa-8cf6-ecf66ded2af3",
+    "salario_promedio_por_sector": "http://localhost:3300/public/question/b7aa9d8b-017d-408d-86c3-fa290383b304", 
+    "salario_promedio_por_pais": "http://localhost:3300/public/question/57dbec53-6cb2-46d8-bfe5-c4259871c5a8", 
+    "salario_promedio_por_genero": "http://localhost:3300/public/question/b499066b-448f-4e22-92b8-102e3504ab96",
+    "salario_promedio_por_cargo": "http://localhost:3300/public/question/ddee8620-fa18-41fb-9f07-9ccf85d1e56a",
+    "promedio_nivel_por_idioma": "http://localhost:3300/public/question/42d9f256-6fe2-4534-bd89-d8e7b117df4f", 
+    "promedio_gasto_salarios_por_sector": "http://localhost:3300/public/question/f9d5de18-470a-4174-a290-a5ef791e4d40", 
+    "palabras_mas_comunes_en_cargos":"http://localhost:3300/public/question/313fcf2e-e214-49ce-922d-ec9bf2bd7257",
+    "nombre_empresas_mas_frecuente":"http://localhost:3300/public/question/ccf2ac9d-928a-414c-9725-a53026b7c6dc",
+    "niveles_idioma_mas_comunes":"http://localhost:3300/public/question/63710329-e80c-45a2-b2fd-aeb35c0cdb2f",
+    "estudiantes_por_pais":"http://localhost:3300/public/question/b27a5211-335c-4767-bb6d-235994bd84d2",
+    "estudiantes_por_genero":"http://localhost:3300/public/question/ccba0e94-db79-4cb2-9087-3a2e0adf25d6",
+    "empresa_por_tipo":"http://localhost:3300/public/question/aa61ff0c-10be-4040-a6c1-8b8a3310a44c",
+    "edad_promedio_egresados":"http://localhost:3300/public/question/0703316f-4e9e-4be0-a35a-fa0ca6e06f2d",  
+    "carreras_mas_comunes_en_empresas":"http://localhost:3300/public/question/e3b4d9f5-ffd9-489c-a27a-9c018368c5d1", 
+    "cantidad_personas_por_genero_y_sector":"http://localhost:3300/public/question/f3f50bec-d6e6-4806-95ca-41896fdd61be",
+    "cantidad_estudiantes_por_idioma":"http://localhost:3300/public/question/3a9c7750-1704-4197-a043-7ebfa98fcbb6",
   }
 
   const totalPages = Math.ceil(filteredData.length / itemsPerPage);
